2a5a5384253f98210de39f7d765600668857b8ef
Tolu Ojo <92734467+Tolu161@users.noreply.github.com>
2023-09-06 14:33:29 +0100

    Add files via upload

---
+++ ArtSellersShopDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtSellersShopDemoApp.swift
+//  ArtSellersShopDemo
+//
+//  Created by Tolu Ojo on 06/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ArtSellersShopDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
